cff6a4461c611b4c9330a642e226bc252d6aaa78
Margheritalavagnini <marghelava@gmail.com>
2023-05-15 14:10:45 +0200

    sincronizza

--- Esercizio_3/3_Neve/sketch.js
+++ Esercizio_3/3_Neve/sketch.js
@@ -1,38 +1,52 @@
-
-function setup(){
-	createCanvas(windowWidth, windowHeight, WEBGL)
-	
-}
-
-
-
-function draw(){
-	
-	// pioggia 1 - per aumentare la quantita di pioggia basta moltiplicare il codice 
-	//finche non si è soddisfatti, si puo fare manualmente o digitalmente scrivendolo un altra loop
-	
-	
-	background(0, 0, 0)
-
-	rotateX(mouseY*0.01)
-
-	let lato = 500
-	
-	for(let i=0; i<20; i=i+1){
-		
-		
-		let lunghezza = random(20,100)
-		let spessore = random(1,3)
-		let posX = random(- lato,lato)
-		let posY = random(- lato,lato)
-		let posZ = random(- lato,lato)
-		
-		
-		stroke(255)
-		strokeWeight(spessore)
-
-		line(posX, posY, posZ, posX, posY, posZ + lunghezza)
+function createSnowflake() {
+	const snowflake = document.createElement("div");
+	snowflake.innerHTML = "&#10052;";
+	snowflake.style.position = "absolute";
+	snowflake.style.top = "-10px";
+	snowflake.style.fontSize = Math.random() * 10 + "px";
+	snowflake.style.color = "#ffffff";
+	snowflake.style.userSelect = "none";
+	snowflake.style.background = "black"; // Aggiunta dello sfondo nero
+	return snowflake;
+  }
+  
+  function animateSnowflake(snowflake) {
+	const start = Math.random() * window.innerWidth;
+	const end = Math.random() * window.innerWidth;
+	const duration = Math.random() * 10 + 5;
+  
+	snowflake.style.left = start + "px";
+  
+	snowflake.animate(
+	  { transform: `translate(${end - start}px, ${window.innerHeight}px)` },
+	  {
+		duration: duration * 1000,
+		easing: "linear",
+		fill: "both",
+		iterations: Infinity,
+		iterationStart: Math.random(),
+	  }
+	);
+  }
+  
+  function startSnowfall() {
+	const snowfallContainer = document.createElement("div");
+	snowfallContainer.style.position = "fixed";
+	snowfallContainer.style.top = "0";
+	snowfallContainer.style.left = "0";
+	snowfallContainer.style.width = "100%";
+	snowfallContainer.style.height = "100%";
+	snowfallContainer.style.pointerEvents = "none";
+	snowfallContainer.style.background = "black"; // Aggiunta dello sfondo nero
+  
+	document.body.appendChild(snowfallContainer);
+  
+	for (let i = 0; i < 50; i++) {
+	  const snowflake = createSnowflake();
+	  snowfallContainer.appendChild(snowflake);
+	  animateSnowflake(snowflake);
 	}
-
-
-}
+  }
+  
+  startSnowfall();
+  // codice suggerito da ChatGPT
